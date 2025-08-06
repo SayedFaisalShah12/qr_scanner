69b63193716588ea1546b3b0c7bd614adb846528
Sayed Faisal Shah <142516641+SayedFaisalShah12@users.noreply.github.com>
2025-08-06 23:47:25 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:device_preview/device_preview.dart';
 import 'package:qr_scaaner/route/app_page.dart';
 import 'package:qr_scaaner/route/app_route.dart';
 
 void main() {
-  runApp(const QRScannerApp());
+  runApp(
+    DevicePreview(
+      enabled: !bool.fromEnvironment('dart.vm.product'), // disabled in release
+      builder: (context) => const QRScannerApp(),
+    ),
+  );
 }
 
 class QRScannerApp extends StatelessWidget {
@@ -15,6 +21,9 @@ class QRScannerApp extends StatelessWidget {
     return GetMaterialApp(
       title: 'QR Scanner App',
       debugShowCheckedModeBanner: false,
+      useInheritedMediaQuery: true, // 👈 required for DevicePreview
+      locale: DevicePreview.locale(context), // 👈 required for locale testing
+      builder: DevicePreview.appBuilder, // 👈 required for wrapping
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
         useMaterial3: true,
