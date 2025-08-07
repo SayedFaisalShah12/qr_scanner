c9866d1c5b06f45ea04774e382d003fc4e2995b8
Sayed Faisal Shah <142516641+SayedFaisalShah12@users.noreply.github.com>
2025-08-08 01:07:28 +0500

    update

--- lib/route/app_page.dart
+++ lib/route/app_page.dart
@@ -1,20 +1,19 @@
 import 'package:get/get.dart';
 import '../binding/qr_scanner_binding.dart';
-import '../views/qr_scanner_screen.dart';
 import '../views/image_picker_screen.dart';
+import '../views/qr_scanner_screen.dart';
 import 'app_route.dart';
 
 class AppPages {
   static final pages = [
     GetPage(
       name: AppRoutes.qrScanner,
-      page: () => const QrScannerScreen(),
+      page: () => const QrScannerView(),
       binding: QrScannerBinding(),
     ),
     GetPage(
       name: '/image-picker',
       page: () => ImagePickerScreen(),
     ),
-
   ];
 }
