d044908180dee406c2194b2b56d5e999e7cc73ee
Sayed Faisal Shah <142516641+SayedFaisalShah12@users.noreply.github.com>
2025-08-08 00:40:16 +0500

    update

---
+++ lib/controllers/image_picker_controller.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ImagePickerController extends GetxController {
+  final Rx<File?> imageFile = Rx<File?>(null);
+  final ImagePicker _picker = ImagePicker();
+
+  Future<void> pickImageFromGallery() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      imageFile.value = File(pickedFile.path);
+    }
+  }
+
+  Future<void> captureImageFromCamera() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.camera);
+    if (pickedFile != null) {
+      imageFile.value = File(pickedFile.path);
+    }
+  }
+}
